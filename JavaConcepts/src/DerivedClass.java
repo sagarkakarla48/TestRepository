74d9b61e556cb32e82b46cbacbcf18befb27ffc2
kakarla sagar babu <kakarlax.kakarla@intel.com>
2019-06-13 12:07:45 +0530

    Test commit 2

--- JavaConcepts/src/DerivedClass.java
+++ JavaConcepts/src/DerivedClass.java
@@ -11,6 +11,7 @@ public class DerivedClass extends BaseClass {
 	public void methodToOverride() //Base class method
     {
          System.out.println ("I'm the method of DerivedClass");
+		 System.out.println ("Test for 2");
     }
 
 }
